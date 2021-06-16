8bb849c85f1eeb39046bb2db0ffd3de772c713c9
Will Bishop <17292672+WillBishop@users.noreply.github.com>
2021-06-16 12:03:00 +0930

    Initial Commit

---
+++ SwiftUIXPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIXPlayground
+//
+//  Created by Will Bishop on 16/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
